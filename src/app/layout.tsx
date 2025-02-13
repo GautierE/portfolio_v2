8996b7f9f27770692f2687106a88567818d9c59d
Gautier <gautier.edel.info@gmail.com>
2025-02-13 22:17:32 +0100

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Gautier EDEL",
+  description: "Gautier EDEL's portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable} dark`}>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}

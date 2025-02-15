72b117a55c25487f0c396c5b544e5a49f89376d4
Gautier <gautier.edel.info@gmail.com>
2025-02-15 09:52:58 +0100

    feat(projects): add more dummy data

--- src/components/projects/data.tsx
+++ src/components/projects/data.tsx
@@ -32,5 +32,86 @@ export const timelineData: TimelineEntry[] = [
             title: "Airbnb like API",
             text: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ul",
         },
+    },
+    {
+        title: "host-haven",
+        projectUrls: {
+            site: {
+                url: "TBD",
+            },
+            github: {
+                url: "https://github.com/GautierE/host-haven"
+            },
+            details: {
+                url: "/host-haven"
+            }
+        },
+        imageUrl: "/images/host-haven-picture.png",
+        tech: [
+            "TypeScript",
+            "NestJS",
+            "Express.js",
+            "Kubernetes",
+            "Microservices architecture",
+            "...",
+        ],
+        card: {
+            title: "Airbnb like API",
+            text: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ul",
+        },
+    },
+    {
+        title: "host-haven",
+        projectUrls: {
+            site: {
+                url: "TBD",
+            },
+            github: {
+                url: "https://github.com/GautierE/host-haven"
+            },
+            details: {
+                url: "/host-haven"
+            }
+        },
+        imageUrl: "/images/host-haven-picture.png",
+        tech: [
+            "TypeScript",
+            "NestJS",
+            "Express.js",
+            "Kubernetes",
+            "Microservices architecture",
+            "...",
+        ],
+        card: {
+            title: "Airbnb like API",
+            text: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ul",
+        },
+    },
+    {
+        title: "host-haven",
+        projectUrls: {
+            site: {
+                url: "TBD",
+            },
+            github: {
+                url: "https://github.com/GautierE/host-haven"
+            },
+            details: {
+                url: "/host-haven"
+            }
+        },
+        imageUrl: "/images/host-haven-picture.png",
+        tech: [
+            "TypeScript",
+            "NestJS",
+            "Express.js",
+            "Kubernetes",
+            "Microservices architecture",
+            "...",
+        ],
+        card: {
+            title: "Airbnb like API",
+            text: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ul",
+        },
     }
 ];
